ce6abcae05e8622917107f500c140f839fd90d54
cromachina <82557197+cromachina@users.noreply.github.com>
2024-06-25 22:10:02 -0400

    Put backups in same relative directories

--- auto_backup.py
+++ auto_backup.py
@@ -15,25 +15,26 @@ backup_directory = pathlib.Path('/home/cro/aux/backups')
 def backup_file(src_path):
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
     src_path = pathlib.Path(src_path)
-    backup_path = backup_directory / f'{src_path.stem}.{timestamp}{src_path.suffix}'
+    backup_path = backup_directory / src_path.relative_to(scan_directory).parent / f'{src_path.stem}.{timestamp}{src_path.suffix}'
     temp_path = backup_path.with_name(f'{backup_path.name}.tmp')
     # Move first because it's atomic. Move back to src to prevent recursive `on_closed` calls.
+    os.makedirs(backup_path.parent, exist_ok=True)
     shutil.move(src_path, backup_path)
     shutil.copy2(backup_path, temp_path)
     shutil.move(temp_path, src_path)
     print(f'backup {src_path} -> {backup_path}')
 
-def clean_backups(src_path):
-    src_stem = pathlib.Path(src_path).stem
-    matcher = re.compile(f'{src_stem}\\.\\d+\\.*')
-    files = os.listdir(backup_directory)
+    # Cleanup old backups.
+    matcher = re.compile(f'{src_path.stem}\\.\\d+\\.*')
+    sub_backup_dir = backup_path.parent
+    files = os.listdir(sub_backup_dir)
     files = [file for file in files if matcher.match(file)]
     if len(files) <= backup_limit:
         return
     files.sort()
     files = files[:-backup_limit]
     for file in files:
-        (pathlib.Path(backup_directory) / file).unlink(missing_ok=True)
+        (pathlib.Path(sub_backup_dir) / file).unlink(missing_ok=True)
 
 class EventHandler(FileSystemEventHandler):
     def __init__(self) -> None:
@@ -46,7 +47,6 @@ class EventHandler(FileSystemEventHandler):
         if src_path.is_relative_to(backup_directory):
             return
         backup_file(src_path)
-        clean_backups(src_path)
 
 observer = Observer()
 observer.schedule(EventHandler(), str(scan_directory), recursive=recursive)
