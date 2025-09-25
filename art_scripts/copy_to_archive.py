9e9cb1e5814175d4780c4f5c0b06ba7553081463
cromachina <82557197+cromachina@users.noreply.github.com>
2025-09-25 12:12:28 -0400

    Update scripts

--- art_scripts/copy_to_archive.py
+++ art_scripts/copy_to_archive.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import shutil
 
-image_types = ['png', 'gif', 'jpg', 'mp4']
-source_types = ['psd', 'clip']
+image_types = ['png', 'gif', 'jpg']
+video_types = ['mp4']
+source_types = ['psd', 'clip', 'kra', 'blend']
 archive = Path('~/aux/archives/current').expanduser()
 keys = archive / 'keys'
 finished = Path('~/aux/art/finished').expanduser()
+timelapse = Path('~/aux/art/timelapse').expanduser()
 temp = Path('~/aux/art/temp').expanduser()
 
 def copy(src_file: Path, dst_dir: Path):
@@ -19,11 +21,25 @@ def arch_dir(file, subname):
     return dir
 
 def main():
+    has_images = False
+
     for type in image_types:
         for file in temp.glob('*.' + type):
+            has_images = True
             images = arch_dir(file, 'images')
             copy(file, images)
             copy(file, finished)
+
+    for type in video_types:
+        for file in temp.glob('*.' + type):
+            if has_images:
+                timelapse_arch = arch_dir(file, 'recordings')
+                copy(file, timelapse_arch)
+                copy(file, timelapse)
+            else:
+                images = arch_dir(file, 'images')
+                copy(file, images)
+                copy(file, finished)
 
     for type in source_types:
         for file in temp.glob('*.' + type):
